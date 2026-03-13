d8405c1589f277cd8fdd3e1fd15cc02d98834aa6
MasutaYunikon <quentinmail974@gmail.com>
2026-03-13 03:47:09 +0100

    fix: workflow directory

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    outDir: 'dist/client'
+  },
   server: {
     port: 3000,
     proxy: {
