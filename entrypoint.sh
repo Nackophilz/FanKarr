7e4b2fa60a5946d3220618f13420a07fa57e8e1b
MasutaYunikon <quentinmail974@gmail.com>
2026-03-20 15:40:28 +0100

    fix: entrypoint.sh config directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ PGID=${PGID:-1000}
 echo "[fankarr] Running as UID=${PUID} GID=${PGID}"
 
 # Corriger les permissions du dossier data
-chown -R "$PUID:$PGID" /app/data
+chown -R "$PUID:$PGID" /config
 
 exec gosu "$PUID:$PGID" node dist/server/index.js
